96ca9e69eecfa84d24c7b00c7c2b5845c2c4097e
Fuchai <huyaojie168@me.com>
2018-03-17 01:39:21 -0500

    CORS and for element of array

--- standaloneflask/fserver.py
+++ standaloneflask/fserver.py
@@ -1,14 +1,23 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/adder')
 def _():
-    num1=int(request.args.get("num1"))
-    num2=int(request.args.get("num2"))
-    res=Response(json.dumps({'result':num1+num2}))
-    res.headers['Content-Type']='application/json'
+    numbers=[]
+    for i in range(6):
+        ret=request.args.get("num"+str(i+1))
+        if ret!=None:
+            numbers.append(int(ret))
+
+    print(numbers)
+
+    answer=sum(numbers)
+    res=Response(json.dumps({'answer': answer}))
+    res.headers['Content-type'] = 'application/json'
     return res
 
 app.run(debug=True, port=5001)
